0aa8acf48bd544a35c693dd91e5b223138dc1f2c
ガンマ技術部 <yu1000.scratch@gmail.com>
2024-12-22 19:34:32 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return[_{{{CITATION{{{_1{](https://github.com/greysonkirk/OfflineBudget/tree/12e332579a3d893effdaeaaddc416949c29b3d49/README.md)
